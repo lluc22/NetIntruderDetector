a1ded645af4000840c4de3de937b4e091b5693ea
Lluc <lluc.bove.canals@gmail.com>
2016-11-05 21:32:43 +0100

    Categorical variables as factors, host_login deleted

--- code/dataExploration.R
+++ code/dataExploration.R
@@ -13,14 +13,14 @@
 # PREPROCESSING 
 ####################################################################
 
-data <- read.csv("data/initialData.csv", header=FALSE, quote="") 
+netData <- read.csv("data/initialData.csv", header=FALSE, quote="") 
 
-dim(data)
+dim(netData)
 
-names(data)
+names(netData)
 #First thing we see that the variables have no name. We fix this:
 
-colnames(data) <- c("duration","protocol_type","service","flag","src_bytes","dst_bytes",
+colnames(netData) <- c("duration","protocol_type","service","flag","src_bytes","dst_bytes",
                         "land","wrong_fragment","urgent","hot","num_failed_logins","logged_in",
                         "num_compromised","root_shell","su_attempted","num_root","num_file_creations",
                         "num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login",
@@ -30,4 +30,28 @@ colnames(data) <- c("duration","protocol_type","service","flag","src_bytes","dst
                         "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
                         "dst_host_rerror_rate","dst_host_srv_rerror_rate","attack_type")
 
+#This variable is our target
+summary(netData$attack_type)
+table(netData$attack_type)
 
+#Delete final dot
+levels(netData$attack_type) <- substr(levels(netData$attack_type),1,nchar(levels(netData$attack_type))-1)
+table(netData$attack_type)
+
+categoricalVars <- c("attack_type","protocol_type","service","flag","land","logged_in","is_host_login","is_guest_login")
+for (c in categoricalVars){print(c);print(table(netData[c]))}
+categoricalVars.index <- which(colnames(netData) %in% categoricalVars)
+
+ #We can remmove "is_host_login" since its values are all the same
+netData <- netData[,-21]
+categoricalVars <- c("attack_type","protocol_type","service","flag","land","logged_in","is_guest_login")
+
+#All Categorical as factors
+netData[categoricalVars] <- lapply(netData[categoricalVars],factor)
+
+#Boolen categoricals with bools
+levels(netData[,"land"]) <- c(F,T)
+levels(netData[,"logged_in"]) <- c(F,T)
+levels(netData[,"is_guest_login"]) <- c(F,T)
+
+summary(netData)
