3180022e01472385791887cfb2450ea0b3f52191
Lluc <lluc.bove.canals@gmail.com>
2016-12-12 21:05:17 +0100

    document

--- code/classification.R
+++ code/classification.R
@@ -25,20 +25,6 @@ table(netData$main_attack,multinom.train)
 table(testData$main_attack,multinom.test)
 
 
-multinom.model <- cv.glmnet(data.matrix(netData),netData$main_attack,family = "multinomial")
-multinom.train <- predict(multinom.model,type="class",newx=data.matrix(netData))
-
-#Training error
-(1 - (sum(diag(table(netData$main_attack,multinom.train))) / length(netData$main_attack))) * 100
-
-#Test Error
-predict(multinom.model,type="class",newx=data.matrix(testData))
-(1 - (sum(diag(table(testData$main_attack,multinom.test))) / length(testData$main_attack))) * 100
-
-table(netData$main_attack,multinom.train)
-#Looks good? look at the confusion matrix
-table(testData$main_attack,multinom.test)
-
 
 #Less frequent classes are less accuratly predicted. To solve this we use undersample the most used class
 library(glmnet)
@@ -46,10 +32,10 @@ library(glmnet)
 bestModel <- NULL
 minCV.error <- Inf
 for (i in 1:100){
-  sub.dos <- sample(row.names(netData[netData$main_attack == "dos",]),60)
-  sub.normal <- sample(row.names(netData[netData$main_attack == "normal",]),60)
-  sub.probe <- sample(row.names(netData[netData$main_attack == "probe",]),60)
-  sub.r2l <- sample(row.names(netData[netData$main_attack == "r2l",]),60)
+  sub.dos <- sample(row.names(netData[netData$main_attack == "dos",]),200)
+  sub.normal <- sample(row.names(netData[netData$main_attack == "normal",]),200)
+  sub.probe <- sample(row.names(netData[netData$main_attack == "probe",]),100)
+  sub.r2l <- sample(row.names(netData[netData$main_attack == "r2l",]),100)
   rows.subNet <- c(row.names(netData[netData$main_attack == "u2r",]),sub.dos,sub.normal,sub.probe,sub.r2l)
   sub.model <- cv.glmnet(data.matrix(netData[rows.subNet,-40]),netData[rows.subNet,"main_attack"],family = "multinomial")
   cve <- tail(sub.model$cvm,n=1)
